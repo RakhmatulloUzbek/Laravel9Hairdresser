4de944a1a0885d794b33a0fcaaa85021af6c315e
RakhmatulloUzbek <rakhmatulloergashevkbu@gmail.com>
2022-05-09 14:00:35 +0300

    Admin Controller Changed

--- app/Http/Controllers/AdminPanel/HomeController.php
+++ app/Http/Controllers/AdminPanel/HomeController.php
@@ -11,4 +11,9 @@ class HomeController extends Controller
     public function index(){
         return view('admin.index');
     }
+
+    public function setting(){
+
+        return view('admin.setting');
+    }
 }
