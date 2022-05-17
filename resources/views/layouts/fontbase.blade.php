bec7dcd4e8c2091a9c6e6c073bbb13d8446642c1
RakhmatulloUzbek <rakhmatulloergashevkbu@gmail.com>
2022-05-17 19:34:00 +0300

    Font page changed

--- resources/views/layouts/fontbase.blade.php
+++ resources/views/layouts/fontbase.blade.php
@@ -2,12 +2,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="BarberShop & Hair Salon HTML Template">
-    <meta name="author" content="DynamicLayers">
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="Rakhmatullo Ergashev">
 
     <title>@yield('title')</title>
 
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets')}}/Design/img/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="@yield('icon')">
+
 
     <!-- Elegant Font Icons CSS -->
     <link rel="stylesheet" href="{{asset('assets')}}/Design/css/elegant-font-icons.css">
@@ -31,21 +33,24 @@
     <link rel="stylesheet" href="{{asset('assets')}}/Design/css/slicknav.min.css">
     <!-- Main CSS -->
     <link rel="stylesheet" href="{{asset('assets')}}/Design/css/main.css">
+
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="{{asset('assets')}}/Design/css/responsive.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <script src="{{asset('assets')}}/Design/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     @yield('head')
 </head>
 <body>
-@include('home.header')
+    @include('home.header')
 
-@yield('slide')
+    @yield('slide')
 
-@yield('content')
+    @yield('content')
 
-@include('home.footer')
+    @include('home.footer')
 
-@yield('foot')
+    @yield('foot')
+
 </body>
 </html>
