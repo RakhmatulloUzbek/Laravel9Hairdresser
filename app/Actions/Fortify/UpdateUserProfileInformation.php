720867ba3621010122051e284bef8472d0e0c8df
RakhmatulloUzbek <rakhmatulloergashevkbu@gmail.com>
2022-06-12 05:46:32 +0300

    UpdateUserProfileInformation Changed

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -35,6 +35,9 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'status' => $input['status'],
+                'phone' => $input['phone'],
+                'address' => $input['address'],
             ])->save();
         }
     }
@@ -51,6 +54,9 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         $user->forceFill([
             'name' => $input['name'],
             'email' => $input['email'],
+            'status' => $input['status'],
+            'phone' => $input['phone'],
+            'address' => $input['address'],
             'email_verified_at' => null,
         ])->save();
 
